e92991e582ad4e64f61e268151b980b5051b208e
Bram Willemsen <lawillem@mit.edu>
2016-05-03 16:26:49 -0400

    First implementation of python wrapper.

---
+++ surfwave_inv/inversion/disp_crv_wrap.py
@@ -0,0 +1,60 @@
+from ctypes import *
+import numpy as np
+import os
+
+def get_disp_curve(freqs, vp_arr, vs_arr, rho_arr, thick_arr, verbose = 0):
+    
+    #get_disp_crv(int N, double* alphas, double* betas, double* rhos, double* ds, double* phase_vels, double* freqs, int nfreqs, int verbose)
+    
+    def load_func(): #Will be called only once. Load the function from the so
+        lib = cdll.LoadLibrary(os.path.dirname(__file__) +'/../sk_disp_crv/_sk_disp_crv.so') #os.path.dirname(__file__) gets path of this wrapper function
+        func = lib.get_disp_crv
+        func.restype  = c_int
+        func.argtypes = [c_int,                                                  #N 
+                         np.ctypeslib.ndpointer(c_double, flags="C_CONTIGUOUS"), #alphas
+                         np.ctypeslib.ndpointer(c_double, flags="C_CONTIGUOUS"), #betas
+                         np.ctypeslib.ndpointer(c_double, flags="C_CONTIGUOUS"), #rhos
+                         np.ctypeslib.ndpointer(c_double, flags="C_CONTIGUOUS"), #ds
+                         np.ctypeslib.ndpointer(c_double, flags="C_CONTIGUOUS"), #phase_vels
+                         np.ctypeslib.ndpointer(c_double, flags="C_CONTIGUOUS"), #freqs
+                         c_int                                                 , #nfreqs
+                         c_int                                                 ] #verbose
+        return func
+    
+    try: #See if we already loaded the shared lib and got func setup
+        func = get_disp_curve.func
+    except: #If not, load and setup
+        func = load_func()
+        get_disp_curve.func = func
+
+    nfreqs = freqs.size
+    N      = vp_arr.size
+        
+    #sanity check
+    if vs_arr.size != N or rho_arr.size != N:
+        raise Exception("Inconsistent material input array sizes")
+
+    if thick_arr.size !=N-1:
+        if thick_arr.size != N or thick_arr[-1] != np.inf:
+            raise Exception("Thickness array not correct. Should either have length N with last value np.inf or size N-1")
+
+    phase_vels = np.zeros(nfreqs, dtype='float64')
+    
+    try: 
+        retval = func(c_int(N), 
+                      np.ascontiguousarray(vp_arr),
+                      np.ascontiguousarray(vs_arr),
+                      np.ascontiguousarray(rho_arr),
+                      np.ascontiguousarray(thick_arr),
+                      np.ascontiguousarray(phase_vels),
+                      np.ascontiguousarray(freqs),
+                      c_int(nfreqs),
+                      c_int(verbose)
+                      ) 
+    except:
+        raise Exception('Unexpected error during execution')
+    
+    if retval == 0:    
+        return phase_vels
+    else:
+        raise Exception("The dispersion curve C-code exited with error code %i"%retval)
